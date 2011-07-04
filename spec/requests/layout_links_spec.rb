afb9f63c3911dfb2dd41a091ed98e54f5ad130f5
Mark Birch <mark@birch.co>
2011-07-03 20:42:12 -0400

    User signup complete

--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -34,7 +34,7 @@ describe "LayoutLinks" do
     click_link "Help"
     response.should have_selector('title', :content => "Help")
     click_link "Contact"
-    response.should have_selector('title', :content => "Content")
+    response.should have_selector('title', :content => "Contact")
     click_link "Home"
     response.should have_selector('title', :content => "Home")
     click_link "Sign up now!"
