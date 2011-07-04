0e6e0bcc140397aa75c2a8bb79b110a6052d22cf
Mark Birch <mark@birch.co>
2011-07-04 15:04:48 -0400

    User sign up form cleanup

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -36,6 +36,7 @@ describe UsersController do
   end
 
   describe "GET 'new'" do
+
     it "should be successful" do
       get :new
       response.should be_success
@@ -44,6 +45,26 @@ describe UsersController do
     it "should have the right title" do
       get :new
       response.should have_selector("title", :content => "Sign up")
+    end
+
+    it "should have a name field" do
+      get :new
+      response.should have_selector("input[name='user[name]'][type='text']")
+    end
+
+    it "should have an email field" do
+      get :new
+      response.should have_selector("input[name='user[email]'][type='text']")
+    end
+
+    it "should have a password field" do
+      get :new
+      response.should have_selector("input[name='user[password]'][type='password']")
+    end
+
+    it "should have a password confirmation field" do
+      get :new
+      response.should have_selector("input[name='user[password_confirmation]'][type='password']")
     end
   end
 
@@ -89,11 +110,12 @@ describe UsersController do
       it "should redirect to the user show page" do
         post :create, :user => @attr
         response.should redirect_to(user_path(assigns(:user)))
+	 end
 
       it "should have a welcome message" do
         post :create, :user => @attr
         flash[:success].should =~ /welcome to the sample app/i
-      end    
+      end
     end
   end
 end
